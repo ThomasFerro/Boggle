0e9123d577f70db22c75c460d0611a083973fa81
Julien Leleu <j.leleu@live.fr>
2015-11-25 16:51:19 +0100

    Classe Dice

---
+++ sources/boggle/words/Dice.java
@@ -0,0 +1,15 @@
+package boggle.words;
+
+public class Dice {
+	
+	char [][] elements;
+	
+	public Dice(int size) {
+		elements = new char [size][6];
+		//TODO : Initialiser avec le dé
+	}
+	
+	public char getValue(int x, int y) {
+		return 0;
+	}
+}
