49dc6a4e78c8beecc0ce0036199e09072833f87e
jujupowaa <j.leleu@live.fr>
2015-11-25 23:08:48 +0100

    Classe Dice et LettersGrid fonctionnel, Génération aléatoire de la grille implémentée !

--- sources/boggle/words/Dice.java
+++ sources/boggle/words/Dice.java
@@ -1,15 +1,67 @@
 package boggle.words;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Random;
+
 public class Dice {
-	
-	char [][] elements;
-	
-	public Dice(int size) {
-		elements = new char [size][6];
-		//TODO : Initialiser avec le dé
+
+	private char[][] elements;
+	private int size;
+
+	public Dice(int size, String pathDice) {
+		this.size = size;
+		elements = new char[size*size][6];
+		initElements(pathDice);
+	}
+
+	public void initElements(String pathDice) {
+		try {
+			BufferedReader fichier_source = new BufferedReader(new FileReader(
+					pathDice));
+			String ligne;
+			int i = 0;
+			while ((ligne = fichier_source.readLine()) != null) {
+				String[] tabChaine = ligne.split(";");			//On decoupe la ligne
+				for (int j = 0; j < elements[0].length; j++) {	
+					elements[i][j] = tabChaine[j].charAt(0);	//On range dans chaque ligne du tableau elements
+				}
+				i++;
+			}
+			fichier_source.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
-	public char getValue(int x, int y) {
-		return 0;
+	public char [][] getElements() {
+		return elements;
 	}
+	
+	public void elementsToString() {
+		for (int i = 0; i < elements.length; i++) {
+			System.out.print(i + "\t");
+			for (int j = 0; j < elements[i].length; j++) {
+				System.out.print(elements[i][j] + " ");
+			}
+			System.out.print("\n");
+		}
+	}
+
+	public char getRandomValue(int x, int y) {
+		Random r = new Random();
+		int indice = x + (y * size);
+		int random = r.nextInt(6);
+		return elements[indice][random];
+	}
+	
+	/*public static void main(String[] args) {
+		Dice d = new Dice (4, "config/des-4x4.csv");
+		d.elementsToString();
+		System.out.println(d.getRandomValue(0, 0));
+	}*/
 }
--- sources/boggle/words/LettersGrid.java
+++ sources/boggle/words/LettersGrid.java
@@ -6,12 +6,19 @@ public class LettersGrid {
 	
 	LettersGrid(int size) {
 		this.size = size;
+		grid = new Letter[size][size];
 		init();
 		//TODO : 
 	}
 	
 	public boolean init() {
-		return false;
+		Dice dice = new Dice(size, "config/des-4x4.csv");
+		for (int i = 0; i < grid.length; i ++) {
+			for (int j = 0; j < grid[i].length; j++) {
+				grid[i][j] = new Letter(dice.getRandomValue(i, j));
+			}
+		}
+		return true;
 	}
 	
 	
@@ -31,5 +38,14 @@ public class LettersGrid {
 	}
 	
 	//-------------------------------------------
-
+	public static void main(String[] args) {
+		LettersGrid l = new LettersGrid(4);
+		Letter[][] grid = l.getGrid();
+		for (int i = 0; i < grid.length; i ++) {
+			for (int j = 0; j < grid[i].length; j++) {
+				System.out.print(grid[i][j].getCharacter());
+			}
+			System.out.println();
+		}
+	}
 }
