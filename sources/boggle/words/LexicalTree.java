a5f7b92cb589807d682c16b5616b0bf37ceb26a9
Thomas Ferro <t.ferro184@gmail.com>
2015-11-27 10:14:04 +0100

    Début LexialTree

--- sources/boggle/words/LexicalTree.java
+++ sources/boggle/words/LexicalTree.java
@@ -1,6 +1,10 @@
 package boggle.words;
 
 import java.util.List ;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 
 /** La classe ArbreLexical permet de stocker de façon compacte et
  * d'accéder rapidement à un ensemble de mots.*/
@@ -11,7 +15,7 @@ public class LexicalTree {
     
     /** Crée un arbre vide (sans aucun mot) */
     public LexicalTree() {
-        // à compléter
+    	this.isWord = false;
     }
 
     /** Indique si le noeud courant est situé à l'extrémité d'un mot
@@ -22,8 +26,13 @@ public class LexicalTree {
     /** Place le mot spécifié dans l'arbre
      * @return <code>true</code> si le mot a été ajouté,
      * <code>false</code> sinon*/
-    public boolean add(String word) {
-        // à compléter
+    public boolean add(String word, int level) {
+        // TODO: à compléter
+    	//Check if the lettre is the last, create a new branch otherwise.
+    	if(word.length() < level) {
+    		
+    		//sons[word.charAt(level)] 
+    	}
         return false ;
     }
 
@@ -32,8 +41,8 @@ public class LexicalTree {
         (String) contenu dans l'arbre, <code>false</code> si
         <code>o</code> n'est pas une instance de String ou si le mot
         n'est pas dans l'arbre lexical. */
-    public boolean contains(String word) {
-        // à compléter
+    public boolean contains(String word, int level) {
+        // TODO: à compléter
         return false ;
     }
 
@@ -42,14 +51,52 @@ public class LexicalTree {
      * @return <code>true</code> si <code>resultat</code> a été
      * modifié, <code>false</code> sinon.*/
     public boolean wordsBeginningWith(String prefix, List<String> result) {
-        // à compléter
+        // TODO: à compléter
         return false ;
     }
     
     /** Crée un arbre lexical qui contient tous les mots du fichier
      * spécifié. */
-    public static LexicalTree readWords(String file) {
-        // à compléter
-        return null ;
+    public static LexicalTree readWords(String filePath) {
+        // TODO: à compléter
+
+    	LexicalTree tree = new LexicalTree();
+    	
+    	//Read the file with BufferedReader : 
+    	BufferedReader br = null;
+    	
+    	try {
+    		String currentLine;
+        	br = new BufferedReader(new FileReader(filePath));
+        	
+        	//Reading..
+        	while((currentLine = br.readLine()) != null) {
+        		//Add the word to the lexical tree:
+        		tree.add(currentLine, 0);
+        	}
+        	
+    	}
+    	catch(IOException e) {
+    		System.out.println(e.getMessage());
+    	}
+    	finally{
+    		try {
+    			if(br != null)
+    				br.close();
+    		}
+    		catch(IOException ex) {
+    			System.out.println(ex.getMessage());
+    		}
+    	}
+    	
+        return tree;
     }
+    
+    public static void main(String[] args) {
+    	LexicalTree tree = LexicalTree.readWords("config/dict-fr.txt");
+    	/*
+    	String test = "test".toUpperCase();
+    	System.out.println((char)(((int)test.charAt(0))));*/
+    }
+    
 }
