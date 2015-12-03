442a91ac7a86a8ee9e4ea49f0401e9b2e6e10897
Julien Leleu <j.leleu@live.fr>
2015-12-03 13:54:32 +0100

    Interface graphique du jeu

---
+++ sources/boggle/gui/gameView/centerPanel/leftPanel/LeftPanel.java
@@ -0,0 +1,41 @@
+package boggle.gui.gameView.centerPanel.leftPanel;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+/**
+ * @author leleuj
+ *
+ */
+public class LeftPanel extends JPanel {
+	
+	private JLabel currentPlayer;
+	private JPanel panelCurrentPlayer;
+	private PanelScore panelScore;
+	
+	public LeftPanel() {
+		currentPlayer = new JLabel("Captain DUCK");
+		panelScore = new PanelScore();
+		panelCurrentPlayer = new JPanel();
+		panelCurrentPlayer.add(new JLabel("Player : "));
+		panelCurrentPlayer.add(currentPlayer);
+		
+		this.setLayout(new BorderLayout());
+		this.setBackground(Color.GRAY);
+		this.add(panelCurrentPlayer, BorderLayout.NORTH);
+		this.add(panelScore, BorderLayout.CENTER);
+		this.setPreferredSize(new Dimension(230,300));
+	}
+
+	public JLabel getCurrentPlayer() {
+		return currentPlayer;
+	}
+
+	public void setCurrentPlayer(String currentPlayer) {
+		this.currentPlayer.setText(currentPlayer);
+	}
+}
---
+++ sources/boggle/gui/gameView/northPanel/NorthPanel.java
@@ -0,0 +1,28 @@
+package boggle.gui.gameView.northPanel;
+
+import java.awt.Color;
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class NorthPanel extends JPanel {
+	
+	private JButton backToMenu;
+	private TimerPanel timer;
+	
+	public NorthPanel() {
+		this.setBackground(Color.CYAN);
+		this.setLayout(new GridLayout(1,3));
+		
+		backToMenu = new JButton("back to menu");
+		timer = new TimerPanel();
+
+		this.add(new JLabel(""));
+		this.add(timer);
+		this.add(backToMenu);
+	}
+
+}
