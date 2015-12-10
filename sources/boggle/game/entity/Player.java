3b9ce7f12f6729bdfd9b823f3576a560dd1fcc21
Thomas Ferro <t.ferro184@gmail.com>
2015-12-10 22:00:12 +0100

    Update Player.java

--- sources/boggle/game/entity/Player.java
+++ sources/boggle/game/entity/Player.java
@@ -2,12 +2,20 @@ package boggle.game.entity;
 
 import java.util.List;
 
+import boggle.game.controller.traitement.MessageRetour;
+
 /**
  * 
  */
-public interface Player extends Entity, Comparable<Player>{
+public interface Player extends Entity, Comparable<Player> {
+	public int compareTo(Player p);
     public int getScore();
     public void setScore(int score);
     public void addWord(String word);
     public List<String> getWords();
+    public void notify(Object o);
+    public boolean isPlaying();
+    public MessageRetour plays();
+    public MessageRetour updateScore(int points);
+    public MessageRetour stopPlaying();
 }
